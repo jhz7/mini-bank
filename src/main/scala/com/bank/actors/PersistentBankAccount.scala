b5e4eca3c8e342d9a0d2829e485542dfe712f120
Jhon Zambrano <jhzambrano7@gmail.com>
2022-04-01 21:06:40 -0500

    Bank account modeling

---
+++ src/main/scala/com/bank/actors/PersistentBankAccount.scala
@@ -0,0 +1,112 @@
+package com.bank.actors
+
+import akka.actor.typed.{ActorRef, Behavior}
+import akka.persistence.typed.PersistenceId
+import akka.persistence.typed.scaladsl.{Effect, EventSourcedBehavior}
+
+/** Represents a single bank account
+  */
+class PersistentBankAccount {
+
+  // Commands
+  sealed trait Command
+
+  final case class CreateBankAccount(
+      user: String,
+      currency: String,
+      initialBalance: BigDecimal,
+      replyTo: ActorRef[Response]
+  ) extends Command
+
+  final case class UpdateBalance(
+      id: String,
+      currency: String,
+      amount: BigDecimal,
+      replyTo: ActorRef[Response]
+  ) extends Command
+
+  final case class GetBankAccount(
+      id: String,
+      replyTo: ActorRef[Response]
+  ) extends Command
+
+  // events
+  sealed trait Event
+
+  final case class BankAccountCreated(bankAccount: BankAccount) extends Event
+
+  final case class BalanceUpdated(amount: BigDecimal) extends Event
+
+  // State
+  final case class BankAccount(
+      id: String,
+      user: String,
+      currency: String,
+      balance: BigDecimal
+  )
+
+  // Responses
+  sealed trait Response
+
+  final case class BankAccountCreatedResponse(id: String) extends Response
+
+  final case class BalanceUpdatedResponse(bankAccount: Option[BankAccount])
+      extends Response
+
+  final case class GetBankAccountResponse(bankAccount: Option[BankAccount])
+      extends Response
+
+  val commandHandler: (BankAccount, Command) => Effect[Event, BankAccount] =
+    (state, command) =>
+      command match {
+        case CreateBankAccount(user, currency, initialBalance, bank) =>
+          val id = state.id
+
+          /*
+          - bank creates me
+          - bank sends me CreateBankAccount
+          - I persist BankAccountCreated
+          - I update my state
+          - I reply back to tha bank with the BankAccountCreatedResponse
+          - (The bank surfaces the response to the HTTP server) thi is later
+           */
+
+          Effect
+            .persist(
+              BankAccountCreated(
+                BankAccount(id, user, currency, balance = initialBalance)
+              )
+            )
+            .thenReply(bank)(_ => BankAccountCreatedResponse(id))
+
+        case UpdateBalance(_, _, amount, bank) =>
+          val newBalance = state.balance + amount
+
+          if (newBalance < 0)
+            Effect.reply(bank)(BalanceUpdatedResponse(None))
+          else
+            Effect
+              .persist(BalanceUpdated(amount))
+              .thenReply(bank)(newState =>
+                BalanceUpdatedResponse(Some(newState))
+              )
+
+        case GetBankAccount(_, bank) =>
+          Effect.reply(bank)(GetBankAccountResponse(Some(state)))
+      }
+
+  val eventHandler: (BankAccount, Event) => BankAccount = (state, event) =>
+    event match {
+      case BankAccountCreated(bankAccount) => bankAccount
+      case BalanceUpdated(amount) =>
+        state.copy(balance = state.balance + amount)
+    }
+
+  def apply(id: String): Behavior[Command] =
+    EventSourcedBehavior[Command, Event, BankAccount](
+      persistenceId = PersistenceId.ofUniqueId(id),
+      emptyState = BankAccount(id, "", "", 0),
+      commandHandler,
+      eventHandler
+    )
+}
