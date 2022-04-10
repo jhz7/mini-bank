03c0ae63ae5c76935a8c9fbb0a177372b19470c9
Jhon Zambrano <jhzambrano7@gmail.com>
2022-04-10 12:15:07 -0500

    Remove unused implicit vals

--- src/main/scala/com/bank/actors/BankPlayground.scala
+++ src/main/scala/com/bank/actors/BankPlayground.scala
@@ -2,11 +2,7 @@ package com.bank.actors
 
 import akka.NotUsed
 import akka.actor.typed.scaladsl.Behaviors
-import akka.actor.typed.{ ActorSystem, Behavior, Scheduler }
-import akka.util.Timeout
-
-import scala.concurrent.ExecutionContext
-import scala.concurrent.duration._
+import akka.actor.typed.{ ActorSystem, Behavior }
 
 object BankPlayground {
 
@@ -15,13 +11,13 @@ object BankPlayground {
   import PersistentBankAccount.Response._
 
   def main(args: Array[String]): Unit = {
-    val rooBehavior: Behavior[NotUsed] = Behaviors.setup { context =>
+    val rootBehavior: Behavior[NotUsed] = Behaviors.setup { context =>
       val logger = context.log
 
       logger.info("BankPlayground started")
 
-      val bank                          = context.spawn(Bank(), "bank")
-      val responseHandler               = context.spawn(
+      val bank            = context.spawn(Bank(), "bank")
+      val responseHandler = context.spawn(
         Behaviors.receiveMessage[Response] {
           case BankAccountCreatedResponse(id) =>
             logger.info(s"BankAccount created with id: $id")
@@ -33,9 +29,6 @@ object BankPlayground {
         },
         "replyHandler"
       )
-      implicit val timeout: Timeout     = Timeout(2.seconds)
-      implicit val scheduler: Scheduler = context.system.scheduler
-      implicit val ec: ExecutionContext = context.executionContext
 
 //      bank ! CreateBankAccount(user = "Jhon", currency = "USD", initialBalance = 10, responseHandler)
       bank ! GetBankAccount(id = "b76fd4c2-67b4-4ff8-a922-9fc94800545b", responseHandler)
@@ -43,7 +36,7 @@ object BankPlayground {
       Behaviors.empty
     }
 
-    val system = ActorSystem(rooBehavior, "BankDemo")
+    val system = ActorSystem(rootBehavior, "BankDemo")
   }
 
 }
